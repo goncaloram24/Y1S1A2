da6f45fafe9386aa76dd4b108193eb4f17c323e3
Gonçalo Moreira <goncaloram@hotmail.com>
2022-10-25 23:32:33 +0100

    SVM_fromscratch.py commmit and push :: something's not right. Didn't finish the video. Stopped it at the 17th minute. Make sure the mistake is corrected before moving on.

--- SVM_fromscratch.py
+++ SVM_fromscratch.py
@@ -6,7 +6,7 @@ style.use('ggplot')
 class Support_Vector_Machine:
     def __init__(self, visualization=True):
         self.visualization = visualization
-        self.color = {1:'r', -1:'b'}
+        self.colors = {1:'r', -1:'b'}
 
         if self.visualization:
             self.fig = plt.figure()
@@ -86,8 +86,45 @@ class Support_Vector_Machine:
     def predict(self, features):
         # sign( x.w+b )
         classification = np.sign(np.dot(np.array(features), self.w) +  self.b)
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
 
         return classification
+
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane = x.w+b
+        # v = x.w+b
+        # psv (positive support vector) = 1
+        # nsv (negative support vector) = -1
+        # dec (decision boundary) = 0
+        def hyperplane(x, w, b, v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2])
+
+        # (w.x+b) = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2])
+
+        # (w.x+b) = 0
+        # decision boundary support vector hyperplane
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2])
+
+        plt.show()
 
 data_dict = {-1:np.array([[1, 7],
                           [2, 8],
@@ -96,3 +133,12 @@ data_dict = {-1:np.array([[1, 7],
              1:np.array([[5, 1],
                         [6,-1],
                         [7, 3],])}
+
+svm = Support_Vector_Machine()
+svm.fit(data=data_dict)
+svm.visualize()
+
+
+
+
+
